aa848ed0ac2eca15bb066e5d9e33bd1c25b4a578
迷茫子 <106359915+Mimangz@users.noreply.github.com>
2022-07-09 23:10:07 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 /*************************
+     Part.0 全局变量…？
+ *************************/
+let enable = 0;    //键盘演奏使能，某些情况下需要关闭使能
+
+/*************************
      Part.I 资源加载函数
  *************************/
-
 let flag = 0, progress = 0, tries = 0;
 let au = [];    //音频列表
 let bgimg = new Image();
@@ -11,7 +15,7 @@ for (let i = 0; i < 21; i++) {
     au[i].src = './audio/' + (i + 1) + '.mp3';
 }
 
-setInterval(() => {    //循环执行，统计加载进度
+let inv = setInterval(() => {    //循环执行，统计加载进度
     flag = 0;    //加载计数器，每个资源计数4
     for (let i = 0; i < 21; i++)
         flag += au[i].readyState;
@@ -40,12 +44,20 @@ setInterval(() => {    //循环执行，统计加载进度
     else {
         document.getElementById('progress-info').innerHTML =    //进度值刷新
             '加载完成！点击派蒙开始演奏吧！ ';
+        clearInterval(inv);    //加载完成后就不刷新了
     }
-}, 200);
+}, 200);    //每秒刷新5次
 
 function enterMain() {
     if (flag == 88) {
         Ani('loading', 'ui-animation');
+        setTimeout(() => {
+            let loading = document.getElementById('loading');
+            if (loading != undefined) {    //保证不会报错
+                loading.parentElement.removeChild(loading);
+            }
+
+        }, 1000);
         enable = 1;    //打开键盘演奏使能
     }
     else {
@@ -61,27 +73,26 @@ function forceEnter() {    //强行进入主界面(草)
 /*************************
     Part.II 核心功能函数
  *************************/
-
-let keyC = {    //键位对照表
+let keyC = {    //键位对照表（采用最新的e.code）
     //原神游戏键位
-    90: '1', 88: '2', 67: '3', 86: '4',
-    66: '5', 78: '6', 77: '7',    //ZXCVBNM
-    65: '8', 83: '9', 68: '10', 70: '11',
-    71: '12', 72: '13', 74: '14',    //ASDFGHJ
-    81: '15', 87: '16', 69: '17', 82: '18',
-    84: '19', 89: '20', 85: '21',    //QWERTYU
+    'KeyZ': '1', 'KeyX': '2', 'KeyC': '3', 'KeyV': '4',
+    'KeyB': '5', 'KeyN': '6', 'KeyM': '7',    //ZXCVBNM
+    'KeyA': '8', 'KeyS': '9', 'KeyD': '10', 'KeyF': '11',
+    'KeyG': '12', 'KeyH': '13', 'KeyJ': '14',    //ASDFGHJ
+    'KeyQ': '15', 'KeyW': '16', 'KeyE': '17', 'KeyR': '18',
+    'KeyT': '19', 'KeyY': '20', 'KeyU': '21',    //QWERTYU
     //扩展键位（数字键）
-    192: '7', 49: '8', 50: '9', 51: '10', 52: '11',
-    53: '12', 54: '13', 55: '14', 56: '15', 57: '16',
-    48: '17', 189: '18', 187: '19', 8: '20',    //~,1~9,0,-,+,bsp
+    'Backquote': '7', 'Digit1': '8', 'Digit2': '9', 'Digit3': '10', 'Digit4': '11',
+    'Digit5': '12', 'Digit6': '13', 'Digit7': '14', 'Digit8': '15', 'Digit9': '16',
+    'Digit0': '17', 'Minus': '18', 'Equal': '19', 'Backspace': '20',    //~,1~9,0,-,+,bsp
     //扩展键位（小键盘）
-    96: '5', 110: '6', 13: '7',    //0 . Enter
-    97: '8', 98: '9', 99: '10', 100: '11', 101: '12',
-    102: '13', 103: '14', 104: '15', 105: '16',    //1~9
-    107: '17', 111: '18', 106: '19', 109: '20'    //+ / * -
-    //暂不使用Num Lock，因为可能会导致奇怪的问题
+    'Numpad0': '5', 'NumpadDecimal': '6', 'NumpadEnter': '7',    //0 . Enter
+    'Numpad1': '8', 'Numpad2': '9', 'Numpad3': '10',
+    'Numpad4': '11', 'Numpad5': '12', 'Numpad6': '13',
+    'Numpad7': '14', 'Numpad8': '15', 'Numpad9': '16',    //1~9
+    'NumpadAdd': '17', 'NumLock': '18', 'NumpadDivide': '19',
+    'NumpadMultiply': '20', 'NumpadSubtract': '21' //+ NumLock / * -
 }
-let enable = 0;    //键盘演奏使能，当有其他界面时默认关闭使能
 
 function notePlay(NID) {    //播放音符
     let music = new Audio();    //创建一个音频对象
@@ -135,9 +146,63 @@ dispCircle();
 
 document.addEventListener(    //使用事件监听器实现键盘操作
     'keydown', function (e) {
-        if (e.repeat)     //防止未松开按键时重复触发
-            return;
-        if (enable == 1)    //只有使能时才允许键盘演奏
-            if (keyC[e.keyCode] != undefined)    //对未定义按键不响应(严谨)
-                playDo(keyC[e.keyCode]);
+        if (e.repeat) { return; }   //防止未松开按键时重复触发
+        if (enable == 1) {   //只有使能时才允许键盘演奏
+            if (keyC[e.code] != undefined) {    //对未定义按键不响应(严谨)
+                playDo(keyC[e.code]);
+            }
+        }
+    })
+
+/*************************
+     Part.III 界面交互函数
+ *************************/
+function hideBox(id) {     //关闭某一个特定的窗口
+    document.getElementById(id).style.display = 'none';
+    if (checkBox() == 0) { enable = 1; }
+}
+
+function showBox(id) {     //打开某一个特定的窗口
+    document.getElementById(id).style.display = 'inline';
+    enable = 0;
+}
+
+function checkBox() {    //检查当前是否有窗口打开
+    let aStyle = document.getElementsByClassName('func-box');
+    let length = aStyle.length;    //设置可变长，提高代码兼容性
+    let shown = 0;    //显示窗口计数
+    for (let i = 0; i < length; i++) {
+        if (aStyle[i].style.display != 'none' &&
+            aStyle[i].style.display != '') { shown++; }
+    }
+    return shown;   //返回窗口打开数
+}
+
+function hideMenuBg() {    //在没有任何窗口的情况下，才能隐藏背景板
+    //其实直接隐藏也可以，但这样做可以避免极端情况下的某些bug
+    let shown = checkBox();    //显示窗口计数
+    if (shown == 0) {
+        document.getElementById('box-bg').
+            style.display = 'none';
+        enable = 1;
+    }
+}
+
+function hideAll() {    //无条件直接关闭所有窗口，隐藏背景板，并打开键盘使能
+    let bB = document.getElementById('box-bg').style;
+    if (bB.display != 'none') {    //有任何窗口显示时才进行关闭
+        let aB = document.getElementsByClassName('func-box');
+        let length = aB.length;
+        bB.display = 'none';
+        for (let i = 0; i < length; i++)
+            if (aB[i].style.display != 'none') { aB[i].style.display = 'none'; }
+        enable = 1;
+    }
+}
+
+document.addEventListener(    //按Esc关闭所有窗口
+    'keydown', function (e) {
+        if (e.code == 'Escape') {
+            hideAll();
+        }
     })
